bbe9f4f9e0c7bdfc96528c704ee48721c8de6be6
Farhan Ferdous <74263768+Farhan41@users.noreply.github.com>
2023-11-18 21:17:47 +0600

    opacity

--- src/Components/Whatsapp.jsx
+++ src/Components/Whatsapp.jsx
@@ -9,7 +9,7 @@ const Whatsapp = () => {
  
       <div className='fixed bottom-[35px] right-[35px]'>
      <Container>
-     <button className=' bg-green-400 p-2 md:p-4 text-white rounded-md'>
+     <button className=' bg-green-400 opacity-80 p-2 md:p-4 text-white rounded-md'>
     <ReactWhatsapp number="+880 1317-791280" message="Hello World!!!">
         <FaWhatsapp className='text-[30px] md:text-[50px]' />
     </ReactWhatsapp>
