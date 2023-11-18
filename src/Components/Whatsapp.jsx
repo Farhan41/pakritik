7d244a2176ce7891aa283257dc2f74edb1ea3914
Farhan Ferdous <74263768+Farhan41@users.noreply.github.com>
2023-11-18 21:23:21 +0600

    number change

--- src/Components/Whatsapp.jsx
+++ src/Components/Whatsapp.jsx
@@ -10,7 +10,7 @@ const Whatsapp = () => {
       <div className='fixed bottom-[35px] right-[35px]'>
      <Container>
      <button className=' bg-green-400 opacity-80 p-2 md:p-4 text-white rounded-md'>
-    <ReactWhatsapp number="+880 1317-791280" message="Hello World!!!">
+    <ReactWhatsapp number="+880 1674-243434" message="I am interested...">
         <FaWhatsapp className='text-[30px] md:text-[50px]' />
     </ReactWhatsapp>
     </button>
