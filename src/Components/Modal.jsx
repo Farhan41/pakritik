297ba5e15b8742dc4667b659ac1b53b2db8ce08d
Farhan Ferdous <74263768+Farhan41@users.noreply.github.com>
2023-11-20 21:40:46 +0600

    spell

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -21,7 +21,7 @@ const Modal = () => {
         (
             <div className='fixed top-0 z-50 w-full h-[100vh] backdrop-blur-md flex justify-center items-center'>
         <div className='gap-y-5 relative w-[300px] h-[200px] bg-white border-4 flex flex-col justify-center items-center'>
-            <h1>This Website is under Construction!</h1> 
+            <h1>This website is under construction!</h1> 
             <AiOutlineClose onClick={handleClick} className='text-[20px] absolute top-3 right-3'/>
             <button onClick={handleClick} className='w-[100px] h-[40px] bg-black text-white rounded-lg'>Continue</button>
         </div>
